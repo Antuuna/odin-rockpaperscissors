253c89dc46ed7439dd0ea8d2746d67452e46b727
Antonio Le <136533712+Antuuna@users.noreply.github.com>
2023-07-11 15:34:22 -0700

    Add singleRound()
    Function prompts for user input then calls getComputerChoice()
    and assigns results to variables. Compares the variables and returns
    a string announcing winner.

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
+let playerSelection;
+let computerSelection;
+
 function getComputerChoice () {
     let random = Math.floor(Math.random() * 3);
         if (random == 0) {
-            return 'Rock';
+            return 'ROCK';
         } else if (random == 1) {
-            return 'Paper';
+            return 'PAPER';
         } else if (random == 2) {
-            return 'Scissors';
+            return 'SCISSORS';
         }
 }
+
+function singleRound (playerSelection, computerSelection) {
+    let result;
+    
+    playerSelection = prompt("Rock, paper, or scissors");
+    computerSelection = getComputerChoice();
+
+    if (playerSelection.toUpperCase() == computerSelection) {
+        result = 'TIE';
+        return ("You " + result + "! " + playerSelection.toUpperCase() + " ties " + computerSelection);
+    }
+
+    if (((playerSelection.toUpperCase() == 'ROCK') && (computerSelection == 'SCISSORS')) ||
+        ((playerSelection.toUpperCase() == 'PAPER') && (computerSelection == 'ROCK')) ||
+        ((playerSelection.toUpperCase() == 'SCISSORS') && (computerSelection == 'PAPER'))) {
+        result = 'WIN';
+        return ("You " + result + "! " + playerSelection.toUpperCase() + " beats " + computerSelection);
+    }
+
+    if (((playerSelection.toUpperCase() == 'ROCK') && (computerSelection == 'PAPER')) ||
+        ((playerSelection.toUpperCase() == 'PAPER') && (computerSelection == 'SCISSORS')) ||
+        ((playerSelection.toUpperCase() == 'SCISSORS') && (computerSelection == 'ROCK'))) {
+        result = 'LOSE';
+        return ("You " + result + "! " + playerSelection.toUpperCase() + " gets beaten by " + computerSelection);
+    }
+}
+
+console.log(singleRound(playerSelection, computerSelection));
